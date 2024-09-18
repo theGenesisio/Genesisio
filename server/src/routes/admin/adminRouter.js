62cf34ad3114207a5585ec415ab40b6287696d1d
Genesisio <genesisioinfo@gmail.com>
2024-09-18 23:12:00 +1200

    fs import

--- server/src/routes/admin/adminRouter.js
+++ server/src/routes/admin/adminRouter.js
@@ -6,6 +6,7 @@ import { adminCreateDeposit, adminDefaultTiers, adminUpdateNetwork, adminUpdateR
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const Router = _Router();
